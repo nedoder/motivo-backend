ef90b6b3d59d6bd0a8b8d83475a9c86224bb56be
nedoder <nedoder89@gmail.com>
2021-07-30 12:10:47 +0200

    testing corrected

--- motivo/tests/test_models.py
+++ motivo/tests/test_models.py
@@ -2,35 +2,35 @@ from django.test import TestCase, Client
 from ..models import Challenge
 from django.urls import reverse
 from ..views import UserEditViewSet
+import json
 from django.contrib.auth.models import User
-client = Client()
+from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory
-factory = APIRequestFactory()
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+# Create your tests here.
+class UserEditTestCase(APITestCase):
+    """ Test case for logging activity"""
+    def setUp(self):
+        # Initialize API client
+        self.client = APIClient()
+        # Create a user
+        self.simpleuser = User.objects.create(username='test', email='test@test.com', first_name='Testy', last_name='Testing')
+        self.simpleuser.set_password("MyPassword")
 
-class testEdit(APITestCase):
-
-    def test_User(self):
-        self.user = User.objects.create(
-            username='test', email='test@test.com', first_name='testing', last_name = 'testtest', password='password')
-
-    def test_editUser(self):
-        factory.put('/user/', {'username':'testnedoder'})
-        self.assertEqual(User.objects.count(), 1)
-        self.assertEqual(User.objects.get().username, 'testnedoder')
-
-class testChallege(TestCase):
-
-    def test_Challenge(self):
-        Challenge.objects.create(
-            title='test', coins_to_win=4, description='testtest'
-        )
-
-    def test_Test(self):
-        try:
-            challenge = Challenge.objects.get(title='test')
-            self.assertEqual(challenge, 'test')
-        except Challenge.DoesNotExist:
-            print('You have no challenge to display')
+        self.simpleuser.save()
+        post_data = {'username':'test', 'password':'MyPassword'}
+        response = self.client.post('/api/token/', post_data)
+        self.token = response.json()['access']
+    def test_user_edit(self):
+        """
+        Test checks if the user was created in the database.
+        """
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        data = {'id': str(self.simpleuser.id), 'username': self.simpleuser.username, 'password': 'MyPassword'}
+        response = self.client.put('/user/', json.dumps(data), content_type='application/json')
+        print(response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- practice/settings.py
+++ practice/settings.py
@@ -130,9 +130,9 @@ STATIC_URL = '/static/'
 
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'TEST_REQUEST_DEFAULT_FORMAT': 'json'
