a273c39f8158a88a1f79685559c40fcd3e646c8d
Mateusz Rędzia <mat.redzia@gmail.com>
2021-08-05 11:11:19 +0200

    Dockerized app with Traefik

---
+++ app/motivo/admin.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+
+from .models import Profile, Challenge, Attempt, Awards
+
+admin.site.register(Profile)
+admin.site.register(Challenge)
+admin.site.register(Attempt)
+admin.site.register(Awards)
+
+
+
---
+++ app/motivo/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+from .models import Profile, Challenge, Attempt
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'initial_budget', 'annual_budget', 'title')
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'collected_coins')
+
+class ChallengeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Challenge
+        fields = ('title', 'coins_to_win', 'description', 'image')
+
+class CompletedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attempt
+        fields = ('challenge', )
+
+class AttemptSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attempt
+        fields = ('user', 'challenge', 'file' )
+
+class UserEditSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email', 'password', 'username' )
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+
+
